7089adec8476e8354b49eb0200b6b9c6fb9bb101
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-15 23:54:51 +0900

    add: operations

--- lib/nodd.dart
+++ lib/nodd.dart
@@ -1,6 +1,31 @@
 import "package:nyxx/nyxx.dart";
 import "dart:io" show Platform,exit,sleep;
+/**
+方針：コマンドは推論型付きマクロ言語となるように構成したい(理想/できれば)
+"@"　型注釈
+":"　コマンド合成/プロパティ接続
+"|"　並列実行
+"+>"　逐次実行
+"?"　nullable　T? = maybe[T] = or[T,null]
+">"　右代入
+"<"　左代入
+"(" ")"　コマンドブロック
 
+型関係：
+anything
+object << anything, null << anything
+number << object,{x@number, y@number}:bind>frac << object,bool << object,string << object,regexp << object,
+domain << object,path << object,uri << object,url << uri,
+userid << object, twitterid << userid,discordid << userid,channelid << object,guilid << object,massageid << object,
+
+ジェネリクス型：
+or[T,U],iterator[T],iterator[K,V],result[R,E],maybe[T]
+
+bind：
+{el1,el2,el3}:bind>some
+some:unbind>{el1,el2,el3}
+
+ */
 void main(List<String> args) {
   Map<String, String> envVars = Platform.environment;
   Nyxx bot = Nyxx(envVars["noddbottoken"], GatewayIntents.allUnprivileged);
---
+++ lib/oper.dart
@@ -0,0 +1,30 @@
+class MacroType{
+  static const TypeAtom Number = TypeAtom.Number;
+  static const TypeAtom Bool = TypeAtom.Bool;
+}
+enum TypeAtom {
+  Number,
+  Bool,
+}
+extension TypeCallAtom on MacroType{
+  @override
+  String toString(){
+    switch(this){
+      case TypeAtom.Number:
+        return "MacroType.Number";
+      case TypeAtom.Bool:
+        return "MacroType.Bool";
+        
+    }
+  }
+}
+class ParamSet{
+  late MacroType _type;
+  late String _name;
+  late Object _value;
+  late bool _required;
+  Paramset(){}
+}
+mixin ParamReq{
+  late ParamSet _param:
+}
