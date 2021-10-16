6f736eef2ccd52ef12e276f3f9c519696cf466be
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-16 13:33:59 +0900

    mod: command substring

--- lib/nodd.dart
+++ lib/nodd.dart
@@ -43,8 +43,8 @@ void main(List<String> args) {
     print("${envVars["nodddbtoken"]}");
   });
   bot.onMessageReceived.listen((event) {
-    String command_this = event.message.content;
-    String prefix_this = command_this.subString(0,1);
+    String command_this = event.message.content.substring(prefix.length);
+    String prefix_this = event.message.content.subString(0,1);
     if (prefix_this == prefix) {
       if(command_this.startsWith("/quit")||command_this.startsWith("/exit")||command_this.startsWith("/kill")){
         event.message.channel.sendMessage(MessageBuilder.content("Nodd System Shutdown."));
