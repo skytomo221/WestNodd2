222d2ef5dedc1f28e72042d9b4554ff9318a0bb0
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-17 21:42:56 +0900

    mod: なるべく型付けるように; add: pollコマンド(仮)

--- lib/nodd.dart
+++ lib/nodd.dart
@@ -32,13 +32,13 @@ some:unbind>{el1,el2,el3}
 
  */
 void main(List<String> args) {
-  final envVars = Platform.environment;
-  final token = envVars["DISCORD_NODD_BOT_TOKEN"];
+  final Map<String,String> envVars = Platform.environment;
+  final String token = envVars["DISCORD_NODD_BOT_TOKEN"];
   if (token == null) {
     throw Exception(
         "Token is not difined. Please set `export DISCORD_NODD_BOT_TOKEN=<TOKEN>`");
   }
-  final guildId = envVars["DISCORD_NODD_GUILD_ID"];
+  final String guildId = envVars["DISCORD_NODD_GUILD_ID"];
   if (guildId == null) {
     throw Exception(
         "Guild ID is not difined. Please set `export DISCORD_NODD_GUILD_ID=<GUILD ID>`");
@@ -56,8 +56,8 @@ void main(List<String> args) {
         guild: guildId.toSnowflake())
       ..registerHandler((event) {
         if (event.args.any((element) => element.name == "text")) {
-          final text = event.getArg("text").value.toString();
-          final catText = text.replaceAll("な", "にゃ");
+          final String text = event.getArg("text").value.toString();
+          final String catText = text.replaceAll("な", "にゃ");
           event.respond(MessageBuilder.content("${catText}にゃ"));
         } else {
           event.respond(MessageBuilder.content("にゃーん"));
@@ -79,13 +79,124 @@ void main(List<String> args) {
         if (RegExp(r'（Ｎｏ．[０-９]+）').hasMatch(rawNick)) {
           event.respond(MessageBuilder.content("そのニックネームには変更できません。"));
         } else {
-          final author = event.interaction.memberAuthor!;
-          final scjId =
+          final Member author = event.interaction.memberAuthor!;
+          final String scjId =
               RegExp(r'（Ｎｏ．[０-９]+）$').firstMatch(author.nickname!)!.group(0)!;
-          final newNick = rawNick + scjId;
+          final String newNick = rawNick + scjId;
           await author.edit(nick: newNick);
           event.respond(MessageBuilder.content("あなたのニックネームを $newNick に変更しました"));
         }
+      ..registerSlashCommand(SlashCommandBuilder(
+        "poll",
+        "投票を開始します。",
+        [
+          CommandOptionBuilder(
+              CommandOptionType.string, "title", "投票のタイトル",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.role, "mention_r", "投票のためのメンション(ロール)",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.user, "mention_m", "投票のためのメンション(メンバ)",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.boolean, "only_mentioned", "投票可能な人をメンションした人に制限するかどうか",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.string, "content", "投票の内容",
+              required: true),
+          CommandOptionBuilder(
+              CommandOptionType.string, "image", "投票につける画像",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.integer, "vote_max", "投票可能な最大数(デフォルトは1)",
+              required: false),
+          CommandOptionBuilder(
+              CommandOptionType.subCommandGroup, "choice", "投票の選択肢",
+              opions:
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_1", "選択肢1",
+                  required: true),
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_2", "選択肢2",
+                  required: true),
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_3", "選択肢3",
+                  required: false),
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_4", "選択肢4",
+                  required: false),
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_5", "選択肢5",
+                  required: false),
+                CommandOptionBuilder(
+                  CommandOptionType.string, "choice_6", "選択肢6",
+                  required: false),
+              required: true),
+        ],
+        guild: guildId.toSnowflake())
+      ..registerHandler((SlashCommandInteractionEvent event) {
+          EnbedBuilder enbeds = EnbedBuilder();
+          late int maxVote;
+          if (event.args.any((InteractionOption element) => element.name == "vote_max")){
+            int temp = int.parse(event.getArg("vote_max").value.toString());
+            if(temp > 0 && temp <= 6){
+              maxVote = temp;
+            }else{
+              maxVote = 1;
+            }
+          }else{
+            maxVote = 1
+          }
+          List<String> choices = {};
+          int choiceNr = 2;
+          choices.add(event.getArg("choice_1").value.toString());
+          choices.add(event.getArg("choice_2").value.toString());
+          if (event.args.any((InteractionOption element) => element.name == "choice_3")) {
+            choices.add(event.getArg("choice_3").value.toString());
+            choiceNr++;
+          }
+          if (event.args.any((InteractionOption element) => element.name == "choice_4")) {
+            choices.add(event.getArg("choice_4").value.toString());
+            choiceNr++;
+          }
+          if (event.args.any((InteractionOption element) => element.name == "choice_5")) {
+            choices.add(event.getArg("choice_5").value.toString());
+            choiceNr++;
+          }
+          if (event.args.any((InteractionOption element) => element.name == "choice_6")) {
+            choices.add(event.getArg("choice_6").value.toString());
+            choiceNr++;
+          }
+          List<EmbedFieldBuilder> retChoices = choices.indexedMap((int index, String choice)=>EmbedFieldBuilder(index, choice)).toList();
+          enbeds.fields = retChoices;
+
+          if (event.args.any((InteractionOption element) => element.name == "only_mentioned")) {
+            strict = bool.parse(event.getArg("only_mentioned").value.toString());
+          }
+          String content = "";
+          if (event.args.any((InteractionOption element) => element.name == "mention_r")) {
+            content += " ";
+            content += event.getArg("mention_r").value.toString();
+          }
+          if (event.args.any((InteractionOption element) => element.name == "mention_m")) {
+            content += " ";
+            content += event.getArg("mention_m").value.toString();
+          }
+          if(content != ""){
+            content += "\n";
+          }
+          content += event.getArg("content").value.toString();
+          enbeds.description = content;
+          final Member author = event.interaction.memberAuthor!;
+          EmbedAuthorBuilder authorR = EmbedAuthorBuilder();
+          authorR.iconUrl = author.avatarURL()!;
+          authorR.name = author.nickname!;
+          enbeds.author = authorR;
+          if (event.args.any((InteractionOption element) => element.name == "image")) {
+            enbeds.imageUrl = event.getArg("image").value.toString();
+          }
+          event.respond(MessageBuilder.enbed(enbeds));
       }))
     ..syncOnReady();
   final Map<String,String> prefixes = {
@@ -93,7 +204,8 @@ void main(List<String> args) {
     "ps": "%",
     "dl": "\$",
   };
-  final String prefix = prefixes["ps"]!;
+  final String prefixKey = "ps";
+  final String prefix = prefixes.containsKey(prefixKey) ? prefixes[prefixKey]! : "";
 
   bot.onReady.listen((ReadyEvent e) {
     print("Ready!");
@@ -120,4 +232,13 @@ void main(List<String> args) {
     }
     print(event.message.content);
   });
+}
+extension IndexedMap<T, E> on List<T> {
+  List<E> indexedMap<E>(E Function(int index, T item) function) {
+    final list = <E>[];
+    asMap().forEach((index, element) {
+      list.add(function(index, element));
+    });
+    return list;
+  }
 }
