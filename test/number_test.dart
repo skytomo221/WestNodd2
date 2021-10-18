2b858a48265c9bbd3833f7cd94659eb854169693
skytomo <tomo1kawa.fullerene@gmail.com>
2021-10-18 18:52:29 +0900

    add: test

--- test/number_test.dart
+++ test/number_test.dart
@@ -1,38 +1,43 @@
 // ignore_for_file: avoid_print
+// ignore: todo
+// TODO: Delete ignore_for_file
 
 import "package:nodd/number.dart";
+import 'package:test/test.dart';
 
 void main() {
-  int n = 86;
-  int rad = 10;
+  test('number test', () async {
+    int n = 86;
+    int rad = 10;
 
-  print("SCJ-Number\n\n");
-  NumGen numgen = NumGen.init(rad, n);
-  print("now?: $n");
-  List<String> now = numgen.past;
-  print(now.last.startsWith("@Deplet") ? "@Deplet" : now.join("\n"));
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
-  print("next: ${++n}");
-  print("\t" + numgen.next);
+    print("SCJ-Number\n\n");
+    NumGen numgen = NumGen.init(rad, n);
+    print("now?: $n");
+    List<String> now = numgen.past;
+    print(now.last.startsWith("@Deplet") ? "@Deplet" : now.join("\n"));
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+    print("next: ${++n}");
+    print("\t" + numgen.next);
+  });
 }
