eef1c86fcd994855b45c4dbd8cc2d0986a0670a9
skytomo <tomo1kawa.fullerene@gmail.com>
2021-10-18 17:04:15 +0900

    mod: format

--- lib/oper.dart
+++ lib/oper.dart
@@ -1,30 +1,33 @@
-class MacroType{
+class MacroType {
   static const TypeAtom Number = TypeAtom.Number;
   static const TypeAtom Bool = TypeAtom.Bool;
 }
+
 enum TypeAtom {
   Number,
   Bool,
 }
-extension TypeCallAtom on MacroType{
+
+extension TypeCallAtom on MacroType {
   @override
-  String toString(){
-    switch(this){
+  String toString() {
+    switch (this) {
       case TypeAtom.Number:
         return "MacroType.Number";
       case TypeAtom.Bool:
         return "MacroType.Bool";
-        
     }
   }
 }
-class ParamSet{
+
+class ParamSet {
   late MacroType _type;
   late String _name;
   late Object _value;
   late bool _required;
-  Paramset(){}
+  Paramset() {}
 }
-mixin ParamReq{
+
+mixin ParamReq {
   late ParamSet _param:
 }
