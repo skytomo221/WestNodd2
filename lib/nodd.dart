daf67184b422319061d9bb198ed742d1ddfb0d3f
skytomo <tomo1kawa.fullerene@gmail.com>
2021-12-18 13:39:24 +0900

    edit: remove comment
    こういうコメントは保守しづらいし、ドキュメントかREADMEに書けばいいので削除したほうがいいと思います

--- lib/nodd.dart
+++ lib/nodd.dart
@@ -7,34 +7,6 @@ import "dart:io" show Platform, exit, sleep;
 
 import 'package:nyxx_interactions/interactions.dart';
 
-/**
-方針：コマンドは推論型付きマクロ言語となるように構成したい(理想/できれば)
-"@"　型注釈
-":"　コマンド合成/プロパティ接続
-"|"　並列実行
-"+>"　逐次実行
-"?"　nullable　T? = maybe[T] = or[T,null]
-">"　右代入
-"<"　左代入
-"(" ")"　コマンドブロック
-
-型関係：
-anything
-object << anything, null << anything
-number << object,{x@number, y@number}:bind>frac << object,bool << object,string << object,regexp << object,
-domain << object,path << object,uri << object,url << uri,
-userid << object, twitterid << userid,discordid << userid,channelid << object,guilid << object,massageid << object,
-
-ジェネリクス型：
-or[T,U],iterator[T],iterator[K,V],result[R,E],maybe[T]
-
-bind：
-{el1,el2,el3}:bind>some
-some:unbind>{el1,el2,el3}
-
-詳細は ../docs/spec/lang.md
-
- */
 void main(List<String> args) {
   final Map<String, String> envVars = Platform.environment;
   final String? token = envVars["DISCORD_NODD_BOT_TOKEN"];
