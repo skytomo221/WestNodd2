3acd35f8e6fc0208bb97b6c4116b5be4e09b09ea
skytomo <tomo1kawa.fullerene@gmail.com>
2021-12-25 16:05:17 +0900

    add: environment variables and scjNumberFormat

--- lib/nodd.dart
+++ lib/nodd.dart
@@ -9,6 +9,7 @@ import "dart:io" show Platform, exit, sleep;
 import 'package:nyxx_interactions/interactions.dart';
 
 void main(List<String> args) {
+  final scjNumberFormat = RegExp(r'#\d{4}$');
   final Map<String, String> envVars = Platform.environment;
   final String? token = envVars["DISCORD_NODD_BOT_TOKEN"];
   if (token == null) {
@@ -20,6 +21,57 @@ void main(List<String> args) {
     throw Exception(
         "Guild ID is not defined. Please set `export DISCORD_NODD_GUILD_ID=<GUILD ID>`");
   }
+  final String? projectId = envVars["DISCORD_NODD_PROJECT_ID"];
+  if (projectId == null) {
+    throw Exception(
+        "Project ID is not defined. Please set `export DISCORD_NODD_PROJECT_ID=<PROJECT ID>`");
+  }
+  final String? privateKeyId = envVars["DISCORD_NODD_PRIVATE_KEY_ID"];
+  if (privateKeyId == null) {
+    throw Exception(
+        "Private key ID is not defined. Please set `export DISCORD_NODD_PRIVATE_KEY_ID=<PRIVATE KEY ID>`");
+  }
+  final String? privateKey = envVars["DISCORD_NODD_PRIVATE_KEY"];
+  if (privateKey == null) {
+    throw Exception(
+        "Private key is not defined. Please set `export DISCORD_NODD_PRIVATE_KEY=<PRIVATE KEY>`");
+  }
+  final String? clientEmail = envVars["DISCORD_NODD_CLIENT_EMAIL"];
+  if (clientEmail == null) {
+    throw Exception(
+        "Client E-mail is not defined. Please set `export DISCORD_NODD_CLIENT_EMAIL=<CLIENT EMAIL>`");
+  }
+  final String? clientId = envVars["DISCORD_NODD_CLIENT_EMAIL"];
+  if (clientId == null) {
+    throw Exception(
+        "Client ID is not defined. Please set `export DISCORD_NODD_CLIENT_ID=<CLIENT ID>`");
+  }
+  final String? clientX509CertUrl =
+      envVars["DISCORD_NODD_CLIENT_X509_CERT_URL"];
+  if (clientX509CertUrl == null) {
+    throw Exception(
+        "The URL of the public x509 certificate is not defined. Please set `export DISCORD_NODD_CLIENT_X509_CERT_URL=<CLIENT X509 CERT URL>`");
+  }
+  final String? spreadsheetId = envVars["DISCORD_NODD_SPREADSHEET_ID"];
+  if (spreadsheetId == null) {
+    throw Exception(
+        "Spreadsheet ID is not defined. Please set `export DISCORD_NODD_SPREADSHEET_ID=<SPREADSHEET ID>`");
+  }
+  final credentials = '''
+{
+  "type": "service_account",
+  "project_id": "$projectId",
+  "private_key_id": "$privateKeyId",
+  "private_key": "$privateKey",
+  "client_email": "$clientEmail",
+  "client_id": "$clientId",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "$clientX509CertUrl"
+}
+''';
+  final numberManager = NumberManager(credentials, spreadsheetId);
   Nyxx bot = Nyxx(token, GatewayIntents.allUnprivileged);
   Interactions(bot)
     ..registerSlashCommand(SlashCommandBuilder(
